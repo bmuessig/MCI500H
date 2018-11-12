2d24ef6baf48fb44541087efeae76af2c06c5912
Benedikt Müssig <benedikt.muessig@gmail.com>
2018-11-13 00:03:48 +0100

    Added CoverCropMode

---
+++ nxgmci/Cover/CoverCropMode.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace nxgmci.Cover
+{
+    public enum CoverCropMode : byte
+    {
+        // Just center the image and don't scale it
+        None,
+
+        // Crops width or height based on the least amount of wasted area
+        MaximizeArea,
+
+        // Crops the image's height to fit
+        CropHeight,
+
+        // Crops the image's width to fit
+        CropWidth,
+
+        // Zooms the image to cover the most area on a black background
+        ZoomBlack,
+
+        // Zooms the image to cover the most area on a white background
+        ZoomWhite,
+
+        // Zooms the image to cover the most area on a gray background
+        ZoomGray,
+
+        // Stretches the image without keeping the original aspect ratio
+        Stretch
+    }
+}
