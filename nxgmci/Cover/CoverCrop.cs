6f08b666171e876f32c60e00ddbec651947653e6
Benedikt Müssig <benedikt.muessig@gmail.com>
2018-11-13 00:04:47 +0100

    Started implementing cover cropping

--- nxgmci/Cover/CoverCrop.cs
+++ nxgmci/Cover/CoverCrop.cs
@@ -2,10 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing;
 
 namespace nxgmci.Cover
 {
     public static class CoverCrop
     {
+        // Global constants
+        private const uint COVER_WIDTH = 320;
+        private const uint COVER_HEIGHT = 240;
+        private const uint THUMB_WIDTH = 48;
+        private const uint THUMB_HEIGHT = 48;
+
+        // The global crop mode
+        public static CoverCropMode CropMode
+        {
+            get;
+            set;
+        }
     }
 }
