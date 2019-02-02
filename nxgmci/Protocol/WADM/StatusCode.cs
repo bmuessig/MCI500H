b6fa689a94dcc6def31f413076bdd50bc69b3224
Benedikt Müssig <benedikt.muessig@gmail.com>
2019-02-03 00:40:19 +0100

    Added documentation to StatusCode

--- nxgmci/Protocol/WADM/StatusCode.cs
+++ nxgmci/Protocol/WADM/StatusCode.cs
@@ -5,15 +5,54 @@
     /// </summary>
     public enum StatusCode
     {
+        /// <summary>
+        /// No status code was returned.
+        /// </summary>
         None,
+
+        /// <summary>
+        /// The operation succeeded.
+        /// </summary>
         Success,
+
+        /// <summary>
+        /// The operation failed.
+        /// </summary>
         Failure,
+
+        /// <summary>
+        /// The operation failed due to an unknown error.
+        /// </summary>
         Unknown,
+
+        /// <summary>
+        /// The device is busy.
+        /// </summary>
         Busy,
+
+        /// <summary>
+        /// The operation failed, as one or more parameters is invalid.
+        /// </summary>
         ParameterError,
+
+        /// <summary>
+        /// The operation failed, as the supplied ID did not match.
+        /// </summary>
         UpdateIDMismatch,
+
+        /// <summary>
+        /// The operation failed, as the supplied index was invalid.
+        /// </summary>
         InvalidIndex,
+
+        /// <summary>
+        /// The operation failed, as the database was full.
+        /// </summary>
         DatabaseFull,
+
+        /// <summary>
+        /// The device is idle.
+        /// </summary>
         Idle
     }
 }
