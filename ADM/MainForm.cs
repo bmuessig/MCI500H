7f37adff16bd9b04489ef1a5faea5909055293e9
Benedikt Müssig <benedikt.muessig@gmail.com>
2019-02-02 18:01:21 +0100

    Updated the test parsing messages

--- ADM/MainForm.cs
+++ ADM/MainForm.cs
@@ -385,13 +385,13 @@ namespace ADM
             Result<WADMProduct> result = parser.Parse(lastResponse, true);
             if (result.Success)
             {
-                if (MessageBox.Show(string.Format("The parsing succeeded!\n{0} root elements and {1} list elements were found.\n\nDo you want to break the program to see the list?",
-                    result.Product.Elements.Count, result.Product.WasList ? result.Product.List.Count : 0), "Success!",
+                if (MessageBox.Show(string.Format("The parsing succeeded after {0}ms!\n{1} root elements and {2} list elements were found.\n\nDo you want to break the program to see the list?",
+                    (int)result.TimeDelta.TotalMilliseconds, result.Product.Elements.Count, result.Product.WasList ? result.Product.List.Count : 0), "Success!",
                     MessageBoxButtons.YesNo, MessageBoxIcon.Information, MessageBoxDefaultButton.Button2) == DialogResult.Yes)
                     ((string)null).ToLower(); // Crash the program
             }
             else
-                MessageBox.Show(string.Format("The parsing FAILED!\n\nError:\n{0}", result.Product.ToString()), "Error!",
+                MessageBox.Show(string.Format("The parsing FAILED after {0}ms!\n\n{1}", (int)result.TimeDelta.TotalMilliseconds, result.ToString()), "Error!",
                     MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
         }
 
