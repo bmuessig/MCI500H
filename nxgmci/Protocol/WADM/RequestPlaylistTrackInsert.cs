340ed40649c2af54e2dd646143cb6961f1568c46
Benedikt Müssig <benedikt@gmx.info>
2019-05-24 07:00:25 +0200

    Implemented RequestPlaylistTrackInsert

--- nxgmci/Protocol/WADM/RequestPlaylistTrackInsert.cs
+++ nxgmci/Protocol/WADM/RequestPlaylistTrackInsert.cs
@@ -68,5 +68,111 @@ namespace nxgmci.Protocol.WADM
                 SourceIndex,
                 Offset); // TODO: Check, if offset can be negative and how it works
         }
+
+        /// <summary>
+        /// Parses RequestPlaylistTrackInsert's ResponseParameters and returns the result.
+        /// </summary>
+        /// <param name="Response">The response received from the stereo.</param>
+        /// <param name="ValidateInput">Indicates whether to validate the data values received.</param>
+        /// <param name="LazySyntax">Indicates whether to ignore minor syntax errors.</param>
+        /// <returns>A result object that contains a serialized version of the response data.</returns>
+        public static Result<ResponseParameters> Parse(string Response, bool ValidateInput = true, bool LazySyntax = false)
+        {
+            // TODO: Check if Index is opitonal and if so, make it so
+
+            // Allocate the result object
+            Result<ResponseParameters> result = new Result<ResponseParameters>();
+
+            // Make sure the response is not null
+            if (string.IsNullOrWhiteSpace(Response))
+                return Result<ResponseParameters>.FailMessage(result, "The response may not be null!");
+
+            // Then, parse the response
+            Result<WADMProduct> parserResult = parser.Parse(Response, LazySyntax);
+
+            // Check if it failed
+            if (!parserResult.Success)
+                if (parserResult.Error != null)
+                    return Result<ResponseParameters>.FailErrorMessage(result, parserResult.Error, "The parsing failed!");
+                else
+                    return Result<ResponseParameters>.FailMessage(result, "The parsing failed for unknown reasons!");
+
+            // Make sure the product is there
+            if (parserResult.Product == null)
+                return Result<ResponseParameters>.FailMessage(result, "The parsing product was null!");
+
+            // And also make sure our state is correct
+            if (parserResult.Product.Elements == null)
+                return Result<ResponseParameters>.FailMessage(result, "The list of parsed elements is null!");
+
+            // Try to parse the status
+            Result<WADMStatus> statusResult = WADMStatus.Parse(parserResult.Product.Elements, ValidateInput);
+
+            // Check if it failed
+            if (!statusResult.Success)
+                if (statusResult.Error != null)
+                    return Result<ResponseParameters>.FailErrorMessage(result, statusResult.Error, "The status code parsing failed!");
+                else
+                    return Result<ResponseParameters>.FailMessage(result, "The status code parsing failed for unknown reasons!");
+
+            // Make sure the product is there
+            if (statusResult.Product == null)
+                return Result<ResponseParameters>.FailMessage(result, "The status code parsing product was null!");
+
+            // Now, make sure our mandatory arguments exist
+            if (!parserResult.Product.Elements.ContainsKey("sourceindex"))
+                return Result<ResponseParameters>.FailMessage(result, "Could not locate parameter '{0}'!", "sourceindex");
+            if (!parserResult.Product.Elements.ContainsKey("updateid"))
+                return Result<ResponseParameters>.FailMessage(result, "Could not locate parameter '{0}'!", "updateid");
+
+            // Then, try to parse the parameters
+            uint sourceIndex, updateID;
+
+            if (!uint.TryParse(parserResult.Product.Elements["sourceindex"], out sourceIndex))
+                return Result<ResponseParameters>.FailMessage(result, "Could not parse parameter '{0}' as uint!", "sourceindex");
+            if (!uint.TryParse(parserResult.Product.Elements["updateid"], out updateID))
+                return Result<ResponseParameters>.FailMessage(result, "Could not parse parameter '{0}' as uint!", "updateid");
+
+            // Finally, return the response
+            return Result<ResponseParameters>.SucceedProduct(result, new ResponseParameters(sourceIndex, statusResult.Product, updateID));
+        }
+
+        /// <summary>
+        /// RequestPlaylistTrackInsert's ResponseParameters reply.
+        /// </summary>
+        public class ResponseParameters
+        {
+            /// <summary>
+            /// The echo of the index of the file to add.
+            /// </summary>
+            public readonly uint SourceIndex;
+
+            /// <summary>
+            /// The status code returned for the query.
+            /// </summary>
+            public readonly WADMStatus Status;
+
+            /// <summary>
+            /// The modification update ID passed as a token. Equal to the originally supplied update ID + 1.
+            /// </summary>
+            public readonly uint UpdateID;
+
+            /// <summary>
+            /// Default internal constructor.
+            /// </summary>
+            /// <param name="SourceIndex">The echo of the index of the file to add.</param>
+            /// <param name="Status">The status code returned for the query.</param>
+            /// <param name="UpdateID">The modification update ID passed as a token. Equal to the originally supplied update ID + 1.</param>
+            internal ResponseParameters(uint SourceIndex, WADMStatus Status, uint UpdateID)
+            {
+                // Sanity check the input
+                if (Status == null)
+                    throw new ArgumentNullException("Status");
+
+                this.SourceIndex = SourceIndex;
+                this.Status = Status;
+                this.UpdateID = UpdateID;
+            }
+        }
     }
 }
